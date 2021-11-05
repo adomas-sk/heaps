528427967ac3b9844381a654fc970f879d49a362
Adomas Skliutas <adomass@wix.com>
2021-11-05 11:03:29 +0200

    Added camera follow

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,5 @@
+import h2d.Camera;
+import h2d.Object;
 import entities.Nest;
 import entities.ResourceBundle;
 import hxd.Math;
@@ -16,22 +18,24 @@ class Main extends hxd.App {
   var resourceBundles: Array<ResourceBundle> = [];
   var nests: Array<Nest> = [];
 
+  var cameraFollow: Object;
+  var movingCamera = false;
+  var lastMousePos = {x: 0., y: 0.};
+
   override function init() {
     // Window.getInstance().vsync = false;
     hxd.Window.getInstance().addEventTarget(moveMouse);
     scene = s2d;
     
-    var background = new Bitmap(h2d.Tile.fromColor(0x666666, 1000, 1000, 1), s2d);
+    //BACKGROUND
+    new Bitmap(h2d.Tile.fromColor(0x666666, 1000, 1000, 1), s2d);
 
     // RESOURCE
     resourceBundles.push(new ResourceBundle(400, 350, 50, 12, s2d));
     resourceBundles.push(new ResourceBundle(500, 500, 50, 8, s2d));
 
-    // NEST
-    // nests.push(new Nest(100, 100));
-
     // CONTROL
-    square = new Bitmap(h2d.Tile.fromColor(0x00FF00, BLOCK_SIZE, BLOCK_SIZE, 0.4), s2d);
+    square = new Bitmap(h2d.Tile.fromColor(0x0099FF, BLOCK_SIZE, BLOCK_SIZE, 0.4), s2d);
     var interaction = new h2d.Interactive(BLOCK_SIZE, BLOCK_SIZE, square);
     interaction.onPush = function(event : hxd.Event) {
       square.alpha = 0.7;
@@ -40,8 +44,16 @@ class Main extends hxd.App {
       square.alpha = 1;
     }
     interaction.onClick = function(event : hxd.Event) {
-      nests.push(new Nest(square.x + BLOCK_SIZE / 2, square.y + BLOCK_SIZE / 2));
-      trace("click!");
+      if (!movingCamera) {
+        var newNestX = square.x + BLOCK_SIZE / 2;
+        var newNestY = square.y + BLOCK_SIZE / 2;
+        for (nest in nests) {
+          if (nest.position.x == newNestX && nest.position.y == newNestY) {
+            return;
+          }
+        }
+        nests.push(new Nest(square.x + BLOCK_SIZE / 2, square.y + BLOCK_SIZE / 2));
+      }
     }
     mouse = new Bitmap(h2d.Tile.fromColor(0xFF0000, 4, 4, 1), s2d);
 
@@ -55,12 +67,36 @@ class Main extends hxd.App {
 
   function moveMouse(event : hxd.Event) {
     switch(event.kind) {
+      case EKeyDown: {
+        if (event.keyCode == 32) {
+          movingCamera = true;
+          square.visible = false;
+        }
+      }
+      case EKeyUp:
+        if (event.keyCode == 32) {
+          movingCamera = false;
+          square.visible = true;
+        }
       case EMove: {
-        mouse.x = event.relX - 2;
-        mouse.y = event.relY - 2;
+        if (
+          movingCamera &&
+          lastMousePos.x != event.relX &&
+          lastMousePos.y != event.relY
+        ) {
+          s2d.interactiveCamera.x -= event.relX - lastMousePos.x;
+          s2d.interactiveCamera.y -= event.relY - lastMousePos.y;
+
+          fpsText.x = s2d.interactiveCamera.x;
+          fpsText.y = s2d.interactiveCamera.y;
+        }
+        mouse.x = s2d.interactiveCamera.x + event.relX - 2;
+        mouse.y = s2d.interactiveCamera.y + event.relY - 2;
 
         square.x = Math.floor(mouse.x / BLOCK_SIZE) * BLOCK_SIZE;
         square.y = Math.floor(mouse.y / BLOCK_SIZE) * BLOCK_SIZE;
+
+        lastMousePos = {x: event.relX, y: event.relY};
       }
       case _:
     }
